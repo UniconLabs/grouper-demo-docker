373fe8e3fd64a9da7f4dffa3d3e0594a31d3d791
John Gasper <jtgasper3@gmail.com>
2016-05-18 18:18:06 -0700

    Update run-grouper.sh

--- opt/container-scripts/run-grouper.sh
+++ opt/container-scripts/run-grouper.sh
@@ -9,7 +9,7 @@ rm -rf /opt/apache-tomcat-6.0.44/work/*
 
 /opt/apache-tomcat-6.0.44/bin/startup.sh
 
-cd /opt/grouper.apiBinary-2.2.2/
+cd /opt/grouper.apiBinary-2.3.0/
 bin/gsh -loader &
 
-tail -f /opt/grouper.apiBinary-2.2.2/logs/grouper_error.log
+tail -f /opt/grouper.apiBinary-2.3.0/logs/grouper_error.log
